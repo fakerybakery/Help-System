fdd32efa4f3e605a0832bfa1cbf7527a7f29f43c
Fakery Bakery <76186054+fakerybakery@users.noreply.github.com>
2021-05-05 14:38:35 -0500

    Update config.php

--- dist/config.php
+++ dist/config.php
@@ -59,6 +59,18 @@ if (mysqli_connect_errno()) {
 ';
     fwrite($myfile, $txt);
     fclose($myfile);
+  
+  $file_pointer = "config.php"; 
+   
+// Use unlink() function to delete a file 
+if (!unlink($file_pointer)) { 
+    echo ("$file_pointer cannot be deleted due to an error"); 
+} 
+else { 
+    echo ("$file_pointer has been deleted"); 
+} 
+  header('Location: index.php');
+  exit;
 }
 ?>
 <!DOCTYPE html>
